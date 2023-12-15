fd9a370f6cd4713d590cfe6de7f737e06b7dcad2
Junior Muniz <117620978+jrpalhano@users.noreply.github.com>
2023-12-15 11:28:56 -0300

    FIX - correção query policial

--- src/Controllers/UsuariosController.js
+++ src/Controllers/UsuariosController.js
@@ -144,9 +144,7 @@ exports.postOfficers = async function ( req, res, err){
         numero_telefone = JSON.parse(req.body.userData).numero_telefone,
         posto_graduacao = JSON.parse(req.body.userData).posto_graduacao,
         batalhao = JSON.parse(req.body.userData).batalhao
-    ]   
-   
-
+    ]
     const queryPolicial = `
     INSERT INTO policiais (
         nome_completo, senha, id_policial, matricula, nome_de_guerra, logradouro, 
@@ -160,7 +158,7 @@ exports.postOfficers = async function ( req, res, err){
         ) VALUES (
             $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,
             $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,
-        $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41 $42
+        $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42
         )
     `
     try {
