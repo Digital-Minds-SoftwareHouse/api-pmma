ddf26d95abc3b6923331756c6e80eaeaff4b2d58
Junior Muniz <117620978+jrpalhano@users.noreply.github.com>
2023-12-21 04:46:11 -0300

    FIX - query de resumo de reprts modificada, eliminando o gr e trazend tods oos dads na requisição

--- src/Controllers/SummaryReportsController.js
+++ src/Controllers/SummaryReportsController.js
@@ -1,39 +1,6 @@
 const postgres = require('../../dbConfig');
 
 exports.getSummaryAllReports = async function (req, res, err){
-
-    console.log('ROTA DE OCORRENCIAS');
-    const reports = (await postgres.query(` SELECT * FROM report`)).rows
-    const reports_natures = (await postgres.query('SELECT * FROM report_nature')).rows
-    const natures = (await postgres.query('SELECT * FROM natures')).rows
-    const response_array = []
-
-    for(let i = 0; i < reports.length; i++){
-        let nature_array = []
-        for(let k = 0; k < reports_natures.length; k++ ){
-            if(reports[i].number_report === reports_natures[k].number_report){
-                nature_array.push(natures[k])
-            }
-        }
-        response_array.push(
-            { 
-                id: reports[i].id,
-                number_report: reports[i].number_report,
-                report_city: reports[i].report_city,
-                type_report: reports[i].type_report,
-                date_time: reports[i].date_time,
-                police_garrison: reports[i].police_garrison,         
-                area: reports[i].area,
-                battalion: reports[i].battalion,
-                natures: nature_array.map(item=>item),
-            }
-        )        
-    }
-    res.status(302).send(response_array)
-}
-exports.reportsPerOfficer = async function (req, res, err){
-    const officerId = req.body.officerId
-    console.log(officerId);
     const query = `
     SELECT r.id, r.number_report, r.battalion ,r.date_time, r.report_city, r.police_garrison, ps.id_policial, string_agg(n.nature, ', ') AS naturezas
     FROM report r
@@ -41,8 +8,22 @@ exports.reportsPerOfficer = async function (req, res, err){
     JOIN police_staff ps ON rs.staff_id = ps.id
     JOIN report_nature rn ON r.number_report = rn.number_report
     JOIN natures n ON rn.nature_id = n.id
-    WHERE ps.id_policial = ${officerId}
     GROUP BY r.id, r.number_report, r.date_time, r.report_city, r.police_garrison, ps.id_policial;
+    `
+    const response = await postgres.query(query)
+
+    res.status(200).send({quantity:response.rowCount, reports: response.rows})
+
+}
+exports.reportsPerOfficer = async function (req, res, err){
+    const officerId = req.body.officerId
+    console.log(officerId);
+    const query = `
+    SELECT r.id, r.number_report, r.date_time, r.report_city, r.police_garrison, string_agg(n.nature, ', ') AS naturezas
+    FROM report r
+    JOIN report_nature rn ON r.number_report = rn.number_report
+    JOIN natures n ON rn.nature_id = n.id
+    GROUP BY r.id, r.number_report, r.date_time, r.report_city, r.police_garrison;
     `
     const response = await postgres.query(query)
 
